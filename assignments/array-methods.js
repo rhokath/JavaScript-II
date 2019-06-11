fd060688b2d230e74335002c05592dc4cbfbecca
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-06-11 18:55:09 -0400

    almost done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,69 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(name){
+    fullName.push(name.first_name + name.last_name);
+})
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+const firstnames = runners.map(function(name) {
+    return allCaps.push(name.first_name.toUpperCase());
+})
+// console.log(firstnames);
 console.log(allCaps); 
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+const biggerShirts = runners.filter(function(shirt){
+    if (shirt.shirt_size === "L"){
+        return true;
+    }
+    
+})
+console.log(biggerShirts);
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(total, donation){
+    return total + donation.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+//The group would like to know who the top donors were in the race so they can get a special goody bag at the end of the race. Provide them with all participants who donated more than $250.
+const topDonors = runners.filter(function(donationSize){
+    if (donationSize.donation > 250){
+        return true;
+    }
+})
+console.log(topDonors);
+const namesTopDonors = topDonors.map(function(name){
+    return name.first_name;
+})
+console.log(namesTopDonors);
 // Problem 2
-
+//The group would like the information orginized in a different way. Please alphebetize by last name!
+const sortedRunners = runners.sort(function(a,b){
+    if(a.last_name > b.last_name){
+        return 1;
+    } else {
+        return -1;
+    }
+});
+console.log(sortedRunners);
 // Problem 3
+//The group just wants a list of companies. Oh yeah alphebetized please!
+let companyNames = []
+runners.forEach(function(company){
+    return companyNames.push(company.company_name)
+})
+console.log(companyNames.sort())
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function saysHello(name) {
+  return function (greeting = "Hello"){
+    return ' ${greeting} , ${name} nice meeting you!';
+
+  }
+}
+saysHello('John')('Hi')
+console.log(saysHello('John')('Hi'));
+const lastName = 'Bond';
+function greet(){
+  const firstName = 'James';
+  return 'The name ${lastName}, ${firstName} {lastName}'
+}
+console.log(greet())
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -8,7 +23,16 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function(){
+    count = count + 1;
+    return count;
+  }
 };
+const newCounter = counter();
+console.log(newCounter())
+console.log(newCounter())
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -17,5 +41,15 @@ const counter = () => {
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  // `decrement` should decrement the counter variable and return it.\
+  let count = 0
+  return Factory = {
+    increment: function(){
+      return ++count;
+    },
+    decrement: function(){
+      return --count;
+    }
+  }
+
 };
